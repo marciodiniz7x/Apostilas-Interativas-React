b2edda0c1a8b458823e717959280d31e71e20f5e
Márcio Diniz <marcio.sccp10@gmail.com>
2023-07-19 16:11:58 -0300

    BaseComponents

---
+++ src/components/basicos/Interface.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Interface() {
+
+
+    return (
+        <div>
+            <h1>Inteface Componente</h1>
+        </div>
+    )
+}
---
+++ src/pages/basicos/interface.tsx
@@ -0,0 +1,10 @@
+import Interface from "@/components/basicos/Interface";
+
+
+export default function interfacehome() {
+    return (
+        <div>
+            <Interface />
+        </div>
+    )
+}
