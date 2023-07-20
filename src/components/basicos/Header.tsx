bb5b83c6c0b77f7ffc5e3349469455ef145fd036
Márcio Diniz <marcio.sccp10@gmail.com>
2023-07-20 13:24:28 -0300

    Update Header.tsx

--- src/components/basicos/Header.tsx
+++ src/components/basicos/Header.tsx
@@ -1,17 +1,42 @@
+import { useRouter } from "next/router"
+
 export default function Header() {
+
+    const router = useRouter();
+
+    const toHome = () => {
+        router.push('interface');
+    }
+
+    const toQuestoes = () => {
+        router.push('questoes-interativas')
+    }
+
+    // const redirectPage = (page) => {
+    //     switch (page) {
+    //         case "home":
+    //             window.location.href = "basicos/interface";
+    //             break;
+        
+    //         default:
+    //             break;
+    //     }
+    // }
+
     return (
         <div className="flex justify-around items-center w-[1200px] h-[100px] bg-white">
 
-                <div className="flex cursor-pointer p-1 text-lg border-2 border-y-indigo-700 border-x-[#f79618]">
+                <div className="flex gap-1 cursor-pointer p-1 text-lg">
                     <span className="text-[#613eff] font-bold">React</span>
                     <span className="text-slate-700 font-normal">Apostilas</span>
                 </div>
 
                 <div className="flex items-center justify-between gap-9 text-slate-700" >
-                    <span className="cursor-pointer hover:text-[#f79618] duration-300">Home</span>
-                    <span className="cursor-pointer hover:text-[#f79618] duration-300">Disciplinas</span>
-                    <span className="cursor-pointer hover:text-[#f79618] duration-300">Exercícios</span>
-                    <span className="cursor-pointer hover:text-[#f79618] duration-300">Sobre</span>
+                    <button 
+                    className="cursor-pointer hover:text-[#f79618] duration-300" onClick={toHome}>Home</button>
+                    <button className="cursor-pointer hover:text-[#f79618] duration-300">Disciplinas</button>
+                    <button className="cursor-pointer hover:text-[#f79618] duration-300" onClick={toQuestoes}>Exercícios</button>
+                    <button className="cursor-pointer hover:text-[#f79618] duration-300">Sobre</button>
                     <button className="cursor-pointer bg-[#f79618] px-7 py-1 rounded-2xl text-white hover:bg-slate-700 duration-700">LOGIN</button>
                 </div>
 
