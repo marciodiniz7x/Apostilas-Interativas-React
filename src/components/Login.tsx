c315841a8f582ff018ed14d98c78b7e5d6c99243
Márcio Diniz <marcio.sccp10@gmail.com>
2023-07-25 17:09:52 -0300

    Update Login.tsx

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -4,10 +4,10 @@ export default function Login() {
 
             <div className="flex bg-white w-[800px] h-[500px] rounded-xl shadow-2xl shadow-[#4dcdff]">
 
-                {/* LEFT */}
+                {/* ESQUERDO */}
                 <div className="w-[60%] h-500px rounded-l-xl"></div>
 
-                {/* RIGHT */}
+                {/* DIREITO */}
                 <div className="flex gap-8 flex-col justify-center items-center w-[350px] h-500px bg-gradient-to-b from-[#5CF4FF] to-[#4DADFF] rounded-r-xl text-white text-center">
                     <span className="text-2xl font-bold">Bem-vindo(a) de volta!</span>
                     <span className="px-12" >Seu progresso não pode parar! Volte e dê continuidade aos seus estudos e upe o seu perfil.</span>
