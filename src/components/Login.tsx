22b43d826f1aa0a8f0394ecc8e5a588a9f73a366
Márcio Diniz <marcio.sccp10@gmail.com>
2023-07-25 16:17:49 -0300

    Update Login.tsx

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,7 +1,15 @@
 export default function Login() {
     return (
-        <div className="flex min-h-[85vh] bg-blue-300">
-            <h1>Login</h1>
+        <div className="interface flex items-center justify-center min-h-[85vh]">
+            <div className={`
+            flex flex-col items-center bg-white h-[500px] w-[400px] p-0
+            shadow-lg rounded-md
+            `}>
+                <div className="flex justify-center items-center w-[400px] h-20 bg-[#4FB9FC] rounded-t-md">
+                    <span className="text-2xl font-bold text-white tex ">Login</span>
+                </div>
+                
+            </div>
         </div>
     )
 }
