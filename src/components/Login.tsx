9d40581d7e334ec4f05ecf44e18745c19bb8df79
Márcio Diniz <marcio.sccp10@gmail.com>
2023-08-05 13:50:36 -0300

    Update Login.tsx

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -5,7 +5,15 @@ export default function Login() {
             <div className="flex bg-white w-[800px] h-[500px] rounded-xl shadow-2xl shadow-[#4dcdff]">
 
                 {/* ESQUERDO */}
-                <div className="w-[60%] h-500px rounded-l-xl"></div>
+                <div className="flex justify-center items-center w-[60%] h-500px rounded-l-xl">
+
+                    <div className="content flex flex-col items-center w-[400px] h-[200px]">
+                        <span className="text-2xl font-bold">E ai!</span>
+                        <span className="px-12" >Entre em sua conta</span>
+                        <input placeholder="Entre" type="text" className="w-[300px] h-[40px] bg-black rounded-3xl shadow-2xl shadow-[#4dcdff]" />
+                        
+                    </div>
+                </div>
 
                 {/* DIREITO */}
                 <div className="flex gap-8 flex-col justify-center items-center w-[350px] h-500px bg-gradient-to-b from-[#5CF4FF] to-[#4DADFF] rounded-r-xl text-white text-center">
